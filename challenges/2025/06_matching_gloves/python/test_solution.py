4d7fa3cd20eb22ec2a973dcdb508435410c159a7
Julen Fuentes <019jfuentes@gmail.com>
2026-01-04 18:57:51 +0100

    Refactor structure for challenge #6 (Code Review section is missing).

--- challenges/2025/06_matching_gloves/python/test_solution.py
+++ challenges/2025/06_matching_gloves/python/test_solution.py
@@ -12,7 +12,7 @@ def test_match_gloves_returns_list():
 
 @pytest.mark.parametrize(
     "gloves,expected_matches", [
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "red"},
                 {"hand": 'R', "color": "red"},
@@ -20,9 +20,10 @@ def test_match_gloves_returns_list():
                 {"hand": 'L', "color": "blue"},
                 {"hand": 'L', "color": "green"}
             ],
-            ["red", "green"]
+            ["red", "green"],
+            id="test-2"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "gold"},
                 {"hand": 'R', "color": "gold"},
@@ -30,26 +31,29 @@ def test_match_gloves_returns_list():
                 {"hand": 'L', "color": "gold"},
                 {"hand": 'R', "color": "gold"}
             ],
-            ["gold", "gold"]
+            ["gold", "gold"],
+            id="test-3"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "red"},
                 {"hand": 'R', "color": "green"},
                 {"hand": 'L', "color": "blue"}
             ],
-            []
+            [],
+            id="test-4"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "green"},
                 {"hand": 'L', "color": "red"},
                 {"hand": 'R', "color": "red"},
                 {"hand": 'R', "color": "green"}
             ],
-            ["red", "green"]
+            ["red", "green"],
+            id="test-5"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'R', "color": "blue"},
                 {"hand": 'L', "color": "blue"},
@@ -57,9 +61,10 @@ def test_match_gloves_returns_list():
                 {"hand": 'L', "color": "blue"},
                 {"hand": 'L', "color": "blue"}
             ],
-            ["blue", "blue"]
+            ["blue", "blue"],
+            id="test-6"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'R', "color": "green"},
                 {"hand": 'L', "color": "red"},
@@ -67,9 +72,10 @@ def test_match_gloves_returns_list():
                 {"hand": 'L', "color": "green"},
                 {"hand": 'L', "color": "red"}
             ],
-            ["red", "green"]
+            ["red", "green"],
+            id="test-7"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "green"},
                 {"hand": 'L', "color": "red"},
@@ -77,9 +83,10 @@ def test_match_gloves_returns_list():
                 {"hand": 'L', "color": "green"},
                 {"hand": 'L', "color": "red"}
             ],
-            []
+            [],
+            id="test-8"
         ),
-        (
+        pytest.param(
             [
                 {"hand": 'L', "color": "silver"},
                 {"hand": 'L', "color": "silver"},
@@ -87,7 +94,8 @@ def test_match_gloves_returns_list():
                 {"hand": 'R', "color": "silver"},
                 {"hand": 'R', "color": "silver"}
             ],
-            ["silver", "silver"]
+            ["silver", "silver"],
+            id="test-9"
         )
     ]
 )
